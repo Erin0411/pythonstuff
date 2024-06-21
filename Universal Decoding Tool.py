3b5b79910c50088857a73aabad194a831f0e2ade
Erin0411 <eringara@proton.me>
2024-06-21 13:58:58 +0200

    Added example

--- Universal Decoding Tool.py
+++ Universal Decoding Tool.py
@@ -1,5 +1,10 @@
 print('Made by Erin, sources: decoders / encoders online, and google. \n\nNo other code was used from the internet or was ai generated\n(because ai is more efficient at coding than me B) )\n\nthis isnt meant to be used as a proper decoder i reccomend using online ones since they may be more accurate\nand theres like also much better decoders you can find on github so thats epic :D\n\n')
 
+
+#EXAMPLE!! USE THIS FOR THE PRESENTATION!!
+# 68 74 74 70 73 3a 2f 2f 77 77 77 2e 79 6f 75 74 75 62 65 2e 63 6f 6d 2f 77 61 74 63 68 3f 76 3d 64 51 77 34 77 39 57 67 58 63 51
+
+
 choice = ''
 while choice.lower() != 'exit':
     choice = input('Choose what to decode\nType "Help" for help\n\nEncoding type, if you dont know, type "Help" for help, Type "Exit" to well, exit.: ')
